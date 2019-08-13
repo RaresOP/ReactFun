56b24d7cc049d8d4ea27829d495b1f6485c00c3a
Rares Zamfiriu <raresz@Mediateldata.local>
2019-08-13 11:08:45 +0300

    first

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ShiftManager = require('./components/ShiftManager.jsx');
+
+ReactDOM.render(<ShiftManager title="AgentID" />, document.getElementById('Shifts'));
